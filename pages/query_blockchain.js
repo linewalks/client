0f191d00ac9d88483c1db576aa558a4538e7d95e
Paul Soh <paulsoh@MacBook-Pro-2.local>
2018-08-22 11:06:23 +0900

    Add boxes for querying blockchain

--- pages/query_blockchain.js
+++ pages/query_blockchain.js
@@ -3,6 +3,14 @@ import PageLayout from "../layout/PageLayout"
 import Ibox from "../component/Ibox"
 
 export default class QueryBlockchain extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      patientsList: []
+    }
+  }
+  getPatientsList = () => {}
+
   render() {
     return (
       <PageLayout>
@@ -12,6 +20,22 @@ export default class QueryBlockchain extends React.Component {
               <div>
                 <h5>Query Registered Patients</h5>
                 <h5>Query Diagnosis and medical claims</h5>
+              </div>
+            </Ibox>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <Ibox title="Get Patient Lists">
+              <div>
+                <div>
+                  <button
+                    className="btn btn-sm btn-primary float-right m-t-n-xs"
+                    onClick={this.getPatientsList}
+                  >
+                    <strong>Retrieve Patient List from Blockchain</strong>
+                  </button>
+                </div>
               </div>
             </Ibox>
           </div>
