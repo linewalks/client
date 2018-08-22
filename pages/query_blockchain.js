b404d2112fb61d7fef3478e08b77abbe2e058c01
Paul Soh <paulsoh@MacBook-Pro-2.local>
2018-08-22 16:29:27 +0900

    Add functionality to retrieve patient list

--- pages/query_blockchain.js
+++ pages/query_blockchain.js
@@ -1,8 +1,10 @@
 import React from "react"
 import PageLayout from "../layout/PageLayout"
 import Ibox from "../component/Ibox"
+import Web3 from "web3"
+import { connect } from "react-redux"
 
-export default class QueryBlockchain extends React.Component {
+class QueryBlockchain extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -10,7 +12,36 @@ export default class QueryBlockchain extends React.Component {
     }
   }
 
-  getPatientsList = () => {}
+  getPatientsList = () => {
+    const { patientContractHash, patientContractAbi } = this.props
+    this.connectAs = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1"
+
+    if (!patientContractHash || !patientContractAbi) {
+      alert(
+        "Patient Query를 하기에 정보가 부족합니다. Patient Tab에 가서 Contract Hash를 기입하세요"
+      )
+    }
+    this.web3 = new Web3(
+      new Web3.providers.HttpProvider("http://localhost:8545")
+    )
+
+    this.patientRegistrarRef = new this.web3.eth.Contract(
+      patientContractAbi,
+      patientContractHash,
+      { from: this.connectAs }
+    )
+    return this.patientRegistrarRef.methods
+      .viewPatientsList()
+      .call()
+      .then(
+        resp => {
+          this.setState({
+            patientsList: [...resp]
+          })
+        },
+        e => console.log(e)
+      )
+  }
 
   render() {
     return (
@@ -39,9 +70,36 @@ export default class QueryBlockchain extends React.Component {
                 </div>
               </div>
             </Ibox>
+            <Ibox title="Patients List">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Patient Address</th>
+                    <th>Year of Birth</th>
+                    <th>Gender</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.patientsList.map((patient, i) => (
+                    <tr>
+                      <td>{i + 1}</td>
+                      <td>{patient[0]}</td>
+                      <td>{patient[1]}</td>
+                      <td>{patient[2]}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </Ibox>
           </div>
         </div>
       </PageLayout>
     )
   }
 }
+
+export default connect(state => ({
+  patientContractHash: state.patientContract.contractHash,
+  patientContractAbi: state.patientContract.abi
+}))(QueryBlockchain)
