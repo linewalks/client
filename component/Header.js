4044b0ea72c977d668e881b948f2a61aba16aa57
Paul Soh <paulsoh@MacBook-Pro-2.local>
2018-08-22 16:03:43 +0900

    Add indicator in Header to display when sufficient info is provided to connect to network

--- component/Header.js
+++ component/Header.js
@@ -1,33 +1,54 @@
+import React from "react"
 import Link from "next/link"
+import { connect } from "react-redux"
+import isEmpty from "lodash/isEmpty"
 
-export default () => (
-  <div className="row border-bottom white-bg">
-    <nav className="navbar navbar-expand-lg navbar-static-top">
-      <Link href="/">
-        <a className="navbar-brand" style={{ color: "white" }}>
-          Medical Claim Issuance and Data Lookup
-        </a>
-      </Link>
-      <div className="navbar-collapse collapse">
-        <ul className="nav navbar-top-links navbar-right">
-          <li className="dropdown">
-            <Link href="/patient">
-              <a className="dropdown-toggle">Patient</a>
-            </Link>
-          </li>
-          <li className="dropdown">
-            <Link href="/provider">
-              <a className="dropdown-toggle">Provider</a>
-            </Link>
-          </li>
+class Header extends React.Component {
+  render() {
+    const patientContractHash = this.props.state.patientContract.contractHash
+    const patientContractAbi = this.props.state.patientContract.abi
+    const isPatientContractInformationSufficient =
+      !!patientContractHash && !isEmpty(patientContractAbi)
+    return (
+      <div className="row border-bottom white-bg">
+        <nav className="navbar navbar-expand-lg navbar-static-top">
+          <Link href="/">
+            <a className="navbar-brand" style={{ color: "white" }}>
+              Medical Claim Issuance and Data Lookup
+            </a>
+          </Link>
+          <div className="navbar-collapse collapse">
+            <ul className="nav navbar-top-links navbar-right">
+              <li className="dropdown">
+                <Link href="/patient">
+                  <a className="dropdown-toggle">
+                    Patient
+                    {isPatientContractInformationSufficient && (
+                      <i
+                        className="fa-circle fa green"
+                        style={{ color: "#1ab394", marginLeft: "4px" }}
+                      />
+                    )}
+                  </a>
+                </Link>
+              </li>
+              <li className="dropdown">
+                <Link href="/provider">
+                  <a className="dropdown-toggle">Provider</a>
+                </Link>
+              </li>
 
-          <li className="dropdown">
-            <Link href="/query_blockchain">
-              <a className="dropdown-toggle">Query Blockchain</a>
-            </Link>
-          </li>
-        </ul>
+              <li className="dropdown">
+                <Link href="/query_blockchain">
+                  <a className="dropdown-toggle">Query Blockchain</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
-    </nav>
-  </div>
-)
+    )
+  }
+}
+
+export default connect(state => ({ state }))(Header)
